fbaba9f7dbecdd6da8a18e5cf35fd4fc3a621ff3
Marisela Castillo <marisela2castillo@gmail.com>
2021-03-18 10:51:27 -0600

    Update Climate.py

--- Climate.py
+++ Climate.py
@@ -47,10 +47,10 @@ def Home():
 #Set precipitations route
 @app.route("/api/v1.0/precipitation")    
 def precipitation():
-    last_yr = dt.date(2017,8,23) - dt.timedelta(days = 365)
+    last_year = dt.date(2017,8,23) - dt.timedelta(days = 365)
     last_day = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
     precipitation = session.query(Measurement.date, Measurement.prcp).\
-        filter(Measurement.date > last_yr).order_by(Measurement.date).all()
+        filter(Measurement.date > last_year).order_by(Measurement.date).all()
 
 #Convert the query results to a dictionary using "date" as the key and "prcp" as the value
     precipitation_data = []
